f8c19ba3fc7e6098742b8e81022452c9cda8e684
Ibne.Khan <ibne.khan@smartbear.com>
2021-01-21 16:34:34 +0100

    Page 21

---
+++ features/support/parameter_types.js
@@ -0,0 +1,6 @@
+const { defineParameterType } = require('@cucumber/cucumber')
+const Coordinate = require('../../lib/coordinate')
+defineParameterType({
+  regexp: /(\d+), (\d+)/,
+  transformer: (x, y) => new Coordinate(parseInt(x), parseInt(y)), name: 'coordinate'
+})
